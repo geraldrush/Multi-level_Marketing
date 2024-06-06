06c49682009dd7d8554c8782bdfb0b835fd28c5d
gerald <gerryrushway@live.com>
2024-06-06 11:42:43 +0200

    [Feature]: Add redis.js

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ import express from "express";
 import bodyParser from "body-parser";
 import userRoutes from "./routes/Users.js";
 import dotenv from "dotenv";
+import RedisStore from "connect-redis";
+import session from "express-session";
+import { createClient } from "redis";
 
 dotenv.config();
 
+// Initialize client.
+let redisClient = createClient();
+redisClient.connect().catch(console.error);
+
+// Initialize store.
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "myapp:",
+});
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+// Initialize session storage.
+app.use(
+  session({
+    store: redisStore,
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      secure: process.env.NODE_ENV === "production", // ensure secure cookies in production
+      maxAge: 1000 * 60 * 60 * 24,
+      name: "mlm", // 1 day
+    },
+  })
+);
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
@@ -20,6 +47,10 @@ app.use(bodyParser.json());
 // Routes
 app.use("/users", userRoutes);
 
+app.get("/test-session", (req, res) => {
+  req.session.test = "Session is working";
+  res.send("Session set");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
